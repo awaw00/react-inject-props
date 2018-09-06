1550e85d0d8ba3351c6d5e87ea92b68510f80add
awaw00 <awaw0618@outlook.com>
2018-09-06 16:22:06 +0800

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { createPropsDecorators } from './createPropsDecorators';
+
+const {InjectProps, ProvideProps, rootContainer} = createPropsDecorators();
+
+export {
+  InjectProps,
+  ProvideProps,
+  rootContainer,
+  createPropsDecorators
+};
