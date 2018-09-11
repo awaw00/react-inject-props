084debf263332d144778019d435a2e7dc8d3d04b
awaw00 <awaw0618@outlook.com>
2018-09-11 16:06:21 +0800

    remove default export InjectProps, ProvideProps...

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { createPropsDecorators } from './createPropsDecorators';
 
-const {InjectProps, ProvideProps, containerManager, ContainerContext} = createPropsDecorators();
-
 export {
-  InjectProps,
-  ProvideProps,
-  containerManager,
-  ContainerContext,
   createPropsDecorators
 };
